68f2cbe86ea0c39f335db9df03a9f295ce38b297
Peter Himschoot <peter@u2u.be>
2024-05-12 20:14:57 +0200

    Working

---
+++ Ch03/BlazorApp.DataBinding/src/BlazorApp.DataBinding/Components/Delay.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorApp.DataBinding.Components;
+
+public class Delay : ComponentBase, IDisposable
+{
+  [Parameter]
+  public double TimeInSeconds { get; set; }
+
+  [Parameter]
+  public EventCallback Tick { get; set; } = default!;
+
+  private Timer timer = default!;
+
+  protected override void OnInitialized()
+  {
+    timer = new(
+      callback: async (_) => await InvokeAsync(async () => await Tick.InvokeAsync()),
+      state: null,
+      dueTime: TimeSpan.FromSeconds(TimeInSeconds),
+      period: Timeout.InfiniteTimeSpan);
+  }
+
+  public void Dispose() => timer.Dispose();
+}
---
+++ Ch03/BlazorApp.DataBinding/src/BlazorApp.DataBinding/Components/Pages/DismissibleAlert.razor.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorApp.DataBinding.Components.Pages;
+
+public partial class DismissibleAlert
+{
+  [Parameter]
+  public bool Show { get; set; }
+
+  [Parameter]
+  public required EventCallback<bool> ShowChanged { get; set; }
+
+  [Parameter]
+  public RenderFragment ChildContent { get; set; } = default!;
+
+  private async Task UpdateShow(bool value)
+  {
+    if (value != Show)
+    {
+      await ShowChanged.InvokeAsync(value);
+    }
+  }
+
+  public async Task Dismiss()
+  => await UpdateShow(false);
+}
