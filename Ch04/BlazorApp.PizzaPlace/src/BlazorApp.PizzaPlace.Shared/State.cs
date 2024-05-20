5c729fd4f382c54025dc4d56e681329b85550bc8
Peter Himschoot <peter@u2u.be>
2024-05-20 17:11:05 +0200

    Ch04

--- Ch04/BlazorApp.PizzaPlace/src/BlazorApp.PizzaPlace.Shared/State.cs
+++ Ch04/BlazorApp.PizzaPlace/src/BlazorApp.PizzaPlace.Shared/State.cs
@@ -3,8 +3,19 @@ public class State
 {
   public Pizza[] Pizzas { get; set; } = [];
   public ShoppingBasket Basket { get; } = new();
+
+  public Customer Customer { get; set; } = new()
+  {
+    Id = 0,
+    Name = string.Empty,
+    Street = string.Empty,
+    City = string.Empty,
+    ZipCode = string.Empty
+  };
+
   public decimal TotalPrice
   => Basket.Orders.Sum(id => GetPizza(id)!.Price);
+
   public Pizza? GetPizza(int id)
   => Pizzas.SingleOrDefault(pizza => pizza.Id == id);
 }
